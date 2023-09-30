d909b8cee5f2e205ab05ee50d6c30ce2b2e6c792
Kenji <70079754+OkayKenji@users.noreply.github.com>
2023-09-30 18:41:29 -0400

    public api + odin data

--- server/someTestingPlsIgnoreMe/README.md
+++ server/someTestingPlsIgnoreMe/README.md
@@ -1,5 +1,5 @@
 To run
-1. Log in to VPN or WiFi with staff creds
-2. npm i (to download all needed node mods)
+1. npm i (to download all needed node mods).
+2. Update dates in history.js as needed
 3. In terminal run: node .\history.js
 4. In ./data/ run whyPython Notebook
--- server/someTestingPlsIgnoreMe/history.js
+++ server/someTestingPlsIgnoreMe/history.js
@@ -10,7 +10,7 @@ outputPort.use(express.static('quickSite'));
 let startYear = "2023";
 let startMonth = "09";
 let startDay = "28";
-let startHour = "00";
+let startHour = "00"; // hours are in GMT, so four/five hours ahead of us
 let startMinute = "00";
 let startSecond = "00";
 
@@ -21,7 +21,8 @@ let endHour = "00";
 let endMinute = "00";
 let endSecond = "00";
 
-let source = "Alan";
+let sourceA = "Odin";
+let sourceB = "Alan";
 
 // don't change
 let utcOffsetHour = "00";
@@ -29,18 +30,45 @@ let utcOffsetMinute = "00";
 
 // console.log(`https://api.bluecolab.cc/influx/sensordata/O/idk/range?stream=false&start_date=${startYear}-${startMonth}-${startDay}T${startHour}%3A${startMinute}%3A${startSecond}}%2B${utcOffsetHour}%3A${utcOffsetMinute}&stop_date=${endYear}-${endMonth}-${endDay}T${endHour}%3A${endMinute}%3A${endSecond}%2B${utcOffsetHour}%3A${utcOffsetMinute}`)
 
-let y = [];
+
 (async () => {
   try {                        
-   const response = await fetch(`https://api.bluecolab.cc/influx/sensordata/${source}/idk/range?stream=false&start_date=${startYear}-${startMonth}-${startDay}T${startHour}%3A${startMinute}%3A${startSecond}%2B${utcOffsetHour}%3A${utcOffsetMinute}&stop_date=${endYear}-${endMonth}-${endDay}T${endHour}%3A${endMinute}%3A${endSecond}%2B${utcOffsetHour}%3A${utcOffsetMinute}`);
+   const response = await fetch(`https://colabprod01.pace.edu/api/influx/sensordata/${sourceA}/idk/range?stream=false&start_date=${startYear}-${startMonth}-${startDay}T${startHour}%3A${startMinute}%3A${startSecond}%2B${utcOffsetHour}%3A${utcOffsetMinute}&stop_date=${endYear}-${endMonth}-${endDay}T${endHour}%3A${endMinute}%3A${endSecond}%2B${utcOffsetHour}%3A${utcOffsetMinute}`);
     if (!response.ok) {
       const error = new Error(`${response.url}: ${response.status} ${response.statusText}`);
       error.response = response;
       throw error;
     }
     const data = await response.json();
-    console.log(data);
-    fs.writeFile('data/myfile.json', JSON.stringify(data,null," "), (err) => {
+    // console.log(data);
+    fs.writeFile(`data/myfile${sourceA}.json`, JSON.stringify(data,null," "), (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('File saved successfully!');
+        }
+      });
+    
+  }
+
+
+  catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+})();
+
+(async () => {
+  try {                        
+   const response = await fetch(`https://colabprod01.pace.edu/api/influx/sensordata/${sourceB}/idk/range?stream=false&start_date=${startYear}-${startMonth}-${startDay}T${startHour}%3A${startMinute}%3A${startSecond}%2B${utcOffsetHour}%3A${utcOffsetMinute}&stop_date=${endYear}-${endMonth}-${endDay}T${endHour}%3A${endMinute}%3A${endSecond}%2B${utcOffsetHour}%3A${utcOffsetMinute}`);
+    if (!response.ok) {
+      const error = new Error(`${response.url}: ${response.status} ${response.statusText}`);
+      error.response = response;
+      throw error;
+    }
+    const data = await response.json();
+    // console.log(data);
+    fs.writeFile(`data/myfile${sourceB}.json`, JSON.stringify(data,null," "), (err) => {
         if (err) {
           console.log(err);
         } else {

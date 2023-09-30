6f7b1ce724563d36009c4cac547d1be5252467e0
Kenji <70079754+OkayKenji@users.noreply.github.com>
2023-09-30 04:23:25 -0400

    Basic Data Viz Test
    See attached instructions in readme

---
+++ server/someTestingPlsIgnoreMe/README.md
@@ -0,0 +1,5 @@
+To run
+1. Log in to VPN or WiFi with staff creds
+2. npm i (to download all needed node mods)
+3. In terminal run: node .\history.js
+4. In ./data/ run whyPython Notebook
---
+++ server/someTestingPlsIgnoreMe/history.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const fs = require('fs');
+
+const outputPort = express();
+outputPort.listen(3030, () => console.log("Server is attempting to startup...serving at localhost:3030"));
+outputPort.use(express.static('quickSite'));
+
+// update these parameters to change days
+let startYear = "2023";
+let startMonth = "09";
+let startDay = "28";
+let startHour = "00";
+let startMinute = "00";
+let startSecond = "00";
+
+let endYear = "2023";
+let endMonth = "09";
+let endDay = "30";
+let endHour = "00";
+let endMinute = "00";
+let endSecond = "00";
+
+// don't change
+let utcOffsetHour = "00";
+let utcOffsetMinute = "00";
+
+// console.log(`https://api.bluecolab.cc/influx/sensordata/Alan/idk/range?stream=false&start_date=${startYear}-${startMonth}-${startDay}T${startHour}%3A${startMinute}%3A${startSecond}}%2B${utcOffsetHour}%3A${utcOffsetMinute}&stop_date=${endYear}-${endMonth}-${endDay}T${endHour}%3A${endMinute}%3A${endSecond}%2B${utcOffsetHour}%3A${utcOffsetMinute}`)
+
+let y = [];
+(async () => {
+  try {                        
+   const response = await fetch(`https://api.bluecolab.cc/influx/sensordata/Alan/idk/range?stream=false&start_date=${startYear}-${startMonth}-${startDay}T${startHour}%3A${startMinute}%3A${startSecond}%2B${utcOffsetHour}%3A${utcOffsetMinute}&stop_date=${endYear}-${endMonth}-${endDay}T${endHour}%3A${endMinute}%3A${endSecond}%2B${utcOffsetHour}%3A${utcOffsetMinute}`);
+    if (!response.ok) {
+      const error = new Error(`${response.url}: ${response.status} ${response.statusText}`);
+      error.response = response;
+      throw error;
+    }
+    const data = await response.json();
+    console.log(data);
+    fs.writeFile('data/myfile.json', JSON.stringify(data,null," "), (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('File saved successfully!');
+        }
+      });
+    
+  }
+
+
+  catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+})();

4470f48c97a0b61af73c1f88206afd83bed0759f
esorbella <92549613+esorbella@users.noreply.github.com>
2023-10-17 16:47:35 -0400

    HOME SCREEN AAAAAAAAAAAAAAAAAA

--- app/sample_app/src/screens/HomeScreen.js
+++ app/sample_app/src/screens/HomeScreen.js
@@ -1,15 +1,82 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
-import styles from '../../styles';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableHighlight,
+  ScrollView,
+  Image,
+  ImageBackground,
+} from "react-native";
+import styles from "../../styles";
+
 const HomeScreen = () => {
   return (
-    <View>
-      <View style = {styles.container}>
-        
+    <ScrollView>
+      <View style={styles.container}>
+        <TouchableHighlight
+          onPress={() => {
+            alert("Pressed Our Story");
+          }}
+        >
+          <View style={styles.buttonContainer}>
+            <Image
+              source={{ uri: "https://i.imgur.com/MMXvBLp.jpg" }}
+              style={{ height: "100%", width: "auto" }}
+            />
+            <View style={styles.mainButton}>
+              <Text style={styles.mainButtonText}>Our Story</Text>
+            </View>
+          </View>
+        </TouchableHighlight>
+        <TouchableHighlight
+          onPress={() => {
+            alert("Pressed Historical Data");
+          }}
+        >
+          <View style={styles.buttonContainer}>
+            <Image
+              source={{ uri: "https://i.imgur.com/MMXvBLp.jpg" }}
+              style={{ height: "100%", width: "auto" }}
+            />
+            <View style={styles.mainButton}>
+              <Text style={styles.mainButtonText}>Historical Data</Text>
+            </View>
+          </View>
+        </TouchableHighlight>
+        <TouchableHighlight
+          onPress={() => {
+            alert("Pressed Local Weather");
+          }}
+        >
+          <View style={styles.buttonContainer}>
+            <Image
+              source={{ uri: "https://i.imgur.com/MMXvBLp.jpg" }}
+              style={{ height: "100%", width: "auto" }}
+            />
+            <View style={styles.mainButton}>
+              <Text style={styles.mainButtonText}>Local Weather</Text>
+            </View>
+          </View>
+        </TouchableHighlight>
+        <TouchableHighlight
+          onPress={() => {
+            alert("Pressed Local Wildlife");
+          }}
+        >
+          <View style={styles.buttonContainer}>
+            <Image
+              source={{ uri: "https://i.imgur.com/MMXvBLp.jpg" }}
+              style={{ height: "100%", width: "auto" }}
+            />
+            <View style={styles.mainButton}>
+              <Text style={styles.mainButtonText}>Choate Pond Wildlife</Text>
+            </View>
+          </View>
+        </TouchableHighlight>
       </View>
-
-    </View>
-  )
+    </ScrollView>
+  );
 };
 
 export default HomeScreen;
--- app/sample_app/styles.js
+++ app/sample_app/styles.js
@@ -11,6 +11,26 @@ export default StyleSheet.create({
         backgroundColor: '#333333',
         alignItems: 'center',
         width: deviceWidth,
-        height: deviceHeight,
+        height: 2*deviceHeight,
+    },
+    mainButton: {
+        borderRadius: 8,
+        height: deviceHeight / 25, 
+        width: deviceWidth / 2,
+        alignItems: 'center',
+        backgroundColor: '#0096DB',
+        borderWidth: 2,
+        borderColor: 'white',
+        justifyContent: 'center',
+    },
+    mainButtonText: {
+        color: 'black',
+    },
+    buttonContainer: {
+        width: deviceWidth / 1.05,
+        borderColor: '#787878',
+        borderWidth: 1,
+        marginTop: deviceHeight / 20,
+        height: deviceHeight / 3,
     }
-})
+});

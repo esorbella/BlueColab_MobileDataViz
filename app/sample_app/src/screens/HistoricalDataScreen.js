682961115f8fe2afd1a6be453dfc7029e989ac8a
ardinkraja02 <ardinkraja02@gmail.com>
2023-10-18 16:25:52 -0400

    git commit

--- app/sample_app/src/screens/HistoricalDataScreen.js
+++ app/sample_app/src/screens/HistoricalDataScreen.js
@@ -5,9 +5,16 @@ import styles from "../../styles";
 
 export default function HistoricalDataScreen({ navigation }) {
   return (
+      <ScrollView>
         <WebView
           source={{ uri: 'https://choatevisual.shinyapps.io/choateVisual/' }}
-          style={{ flex: 1 }}
+          style={{ flex: .5 }}
         />
+        <View style = {styles.container}>
+          <Text>
+            Animals here
+          </Text>
+        </View>
+      </ScrollView>
   );
 }

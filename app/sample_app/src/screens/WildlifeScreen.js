c1d030aae921ab0b65cb0b83515b93ce433db8e0
ardinkraja02 <ardinkraja02@gmail.com>
2023-11-14 16:34:16 -0500

    i broke wildlife screeen by accident fixed now

--- app/sample_app/src/screens/WildlifeScreen.js
+++ app/sample_app/src/screens/WildlifeScreen.js
@@ -15,7 +15,60 @@ import styles from "../../styles";
 const windowWidth = Dimensions.get("window").width;
 
 const WildlifeScreen = ({ navigation }) => {
-  const wildlifeData = [
+  const choateWildlifeData = [
+    {
+      label: "Largemouth bass (Micropterus salmoides)",
+      imageUri:
+        "https://th.bing.com/th/id/R.6dc550e4629145cbd44548c0b36a6145?rik=gHN9%2bthmmTih9A&riu=http%3a%2f%2fupload.wikimedia.org%2fwikipedia%2fcommons%2fe%2fed%2fLargemouth_bass_fish_underwater_animal_in_natural_habitat_micropterus_salmoides.jpg&ehk=BjhvQX7mchDiAAH0iVqhZ7QGdUjUlcL9SFFuNcRo6PM%3d&risl=&pid=ImgRaw&r=0",
+    },
+    {
+      label: "Snapping turtle (Chelydra serpentina)",
+      imageUri:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Common_Snapping_Turtle_Close_Up.jpg/1280px-Common_Snapping_Turtle_Close_Up.jpg",
+    },
+    {
+      label: "Red-Eared Sliders(Trachemys scripta elegans)",
+      imageUri:
+        "https://www.thesprucepets.com/thmb/v96sCG04MaXcJJMoiB_sjH-846U=/5500x0/filters:no_upscale():strip_icc()/red-eared-slider-swimming-520669620-57fff0185f9b5805c2b11b7b.jpg",
+    },
+    {
+      label: "Goldfish (Carassius auratus)",
+      imageUri:
+        "https://image.mlive.com/home/mlive-media/pgfull/img/grandrapidspress/photo/2016/04/26/-22847150d92e4f23.JPG",
+    },
+    {
+      label: "Wood Ducks (Aix sponsa)",
+      imageUri:
+        "https://www.allaboutbirds.org/guide/assets/photo/65533521-480px.jpg",
+    },
+    {
+      label: "Great Blue Heron (Ardea herodias)",
+      imageUri:
+        "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/60314281/1800",
+    },
+    {
+      label: "CrayFish (Orconectes rusticus)",
+      imageUri:
+        "https://fullserviceaquatics.com/wp-content/uploads/2012/04/pond_crayfish_main.jpg",
+    },
+    {
+      label: "Northern Water Snake (Nerodia sipedon)",
+      imageUri:
+        "https://www.mass.gov/files/styles/embedded_full_width/public/images/2022-08/northernwatersnakstockcrop_0.jpg?itok=aSzaOBR7",
+    },
+    {
+      label: "Green Frog (Rana clamitans)",
+      imageUri:
+        "https://www.thesprucepets.com/thmb/lyR-o9nIgJRke0rZOEPu5j2gIxY=/2122x0/filters:no_upscale():strip_icc()/95724120-56a2bcee5f9b58b7d0cdf874.jpg",
+    },
+    {
+      label: "Bullfrog (Lithobates catesbeianus)",
+      imageUri:
+        "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/1-bullfrog-smithtown-new-york-bob-savage.jpg",
+    },
+    // Add more wildlife data in a similar format
+  ];
+  const hudsonWildlifeData = [
     {
       label: "Largemouth bass (Micropterus salmoides)",
       imageUri:
@@ -81,14 +134,22 @@ const WildlifeScreen = ({ navigation }) => {
 
   return (
     <ScrollView>
-      <View style={styles.wildContainer}>
-        <Text style = {styles.paragraphText}> Wildlife in Choate Pond: </Text>
-        <Image
-          source={{
-            uri: "https://th.bing.com/th/id/R.6dc550e4629145cbd44548c0b36a6145?rik=gHN9%2bthmmTih9A&riu=http%3a%2f%2fupload.wikimedia.org%2fwikipedia%2fcommons%2fe%2fed%2fLargemouth_bass_fish_underwater_animal_in_natural_habitat_micropterus_salmoides.jpg&ehk=BjhvQX7mchDiAAH0iVqhZ7QGdUjUlcL9SFFuNcRo6PM%3d&risl=&pid=ImgRaw&r=0",
-          }}
-          style={{ width: windowWidth * 0.9, aspectRatio: 2097 / 1227 }}
-          // Set width to 90% of screen width and maintain aspect ratio
+      <View style={styles.container}>
+        <Text style={styles.paragraphText}>Wildlife in Choate Pond:</Text>
+        <Carousel
+          data={choateWildlifeData}
+          renderItem={renderItem}
+          sliderWidth={windowWidth}
+          itemWidth={windowWidth * 0.9}
+          layout="default"
+        />
+        <Text style={styles.paragraphText}>Wildlife in The Hudson River:</Text>
+        <Carousel
+          data={hudsonWildlifeData}
+          renderItem={renderItem}
+          sliderWidth={windowWidth}
+          itemWidth={windowWidth * 0.9}
+          layout="default"
         />
       </View>
     </ScrollView>

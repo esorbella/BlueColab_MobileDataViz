df57002eccac0ac3155de13511a75244a279f3c2
ardinkraja02 <ardinkraja02@gmail.com>
2023-11-15 15:41:07 -0500

    fixed app.js

--- app/sample_app/App.js
+++ app/sample_app/App.js
@@ -11,22 +11,10 @@ import DataYonk from "./src/screens/Data screens/DataYonk";
 import WeatherScreen from "./src/screens/WeatherScreen";
 import StoryScreen from "./src/screens/StoryScreen";
 import DataHub from "./src/screens/DataHub";
-import React, { useEffect, useState } from 'react'
 import WildlifeScreen from "./src/screens/WildlifeScreen";
 import { AppLoading } from 'expo';
 import * as Font from 'expo-font';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import HomeScreen from './src/screens/HomeScreen';
-import WelcomeScreen from './src/screens/WelcomeScreen';
-import DataChoate from './src/screens/Data screens/DataChoate';
-import DataPough from './src/screens/Data screens/DataPough';
-import DataWP from './src/screens/Data screens/DataWP';
-import DataYonk from './src/screens/Data screens/DataYonk';
-import WeatherScreen from './src/screens/WeatherScreen';
-import StoryScreen from './src/screens/StoryScreen';
-import DataHub from './src/screens/DataHub';
-import WildlifeScreen from './src/screens/WildlifeScreen';
+
 
 const fetchFonts = () => {
   return Font.loadAsync({

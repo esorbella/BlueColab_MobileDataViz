a053bf7476c478d55125b2b85b96b961ca77dc24
ardinkraja02 <ardinkraja02@gmail.com>
2023-12-04 17:38:12 -0500

    BLUEJ JUNIT TEST SETUP

---
+++ jUnitTest/address-book-junit-copy/SensorDataList.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+/**
+ * Write a description of class SensorDataList here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class SensorDataList
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    
+    ArrayList<Sensor> sList = new ArrayList<Sensor>(); // Create an ArrayList object
+
+    /**
+     * Constructor for objects of class SensorDataList
+     */
+    public SensorDataList()
+    {
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int search(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+    
+}

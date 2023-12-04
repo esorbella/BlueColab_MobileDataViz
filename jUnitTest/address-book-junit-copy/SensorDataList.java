bd6beeb66b017cc00957e2a0ab331ed3594c88e4
ardinkraja02 <ardinkraja02@gmail.com>
2023-12-04 17:52:56 -0500

    More JUNIT changes

--- jUnitTest/address-book-junit-copy/SensorDataList.java
+++ jUnitTest/address-book-junit-copy/SensorDataList.java
@@ -10,26 +10,18 @@ public class SensorDataList
     // instance variables - replace the example below with your own
     private int x;
     
-    ArrayList<Sensor> sList = new ArrayList<Sensor>(); // Create an ArrayList object
+    private ArrayList<Sensor> sList; // Create an ArrayList object
 
     /**
      * Constructor for objects of class SensorDataList
      */
     public SensorDataList()
     {
-        
+        sList = new ArrayList<>();
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int search(int y)
-    {
-        // put your code here
-        return x + y;
+  
+    public ArrayList getList(){
+        return sList;
     }
     
 }
